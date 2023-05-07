12b00bf862cb3932fd43d28fe861aab2e3bd564d
Hirschiii <niklas@vonhirschfeld.eu>
2023-05-07 15:53:45 +0200

    auto updating site

--- _site/autopush_uberspace.sh
+++ _site/autopush_uberspace.sh
@@ -5,4 +5,4 @@ jekyll build
 rsync -arP --delete _site/ gulugulu@aegaeon.uberspace.de:/home/gulugulu/html/vonhirschfeld.eu/niklas/
 git add .
 git commit -m "auto updating site"
-git push
+# git push
