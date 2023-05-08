c862fa8fe468c13d652ee2084da4953c5b26f78d
Hirschiii <niklas@vonhirschfeld.eu>
2023-05-08 18:51:34 +0200

    auto updating site

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 jekyll build
 
-rsync -arP --delete _site/ gulugulu@aegaeon.uberspace.de:/home/gulugulu/html/vonhirschfeld.eu/niklas/
+sshpass -p "$(pass gulugulu@aegaeon.uberspace.de)" rsync -arP --delete _site/ gulugulu@aegaeon.uberspace.de:/home/gulugulu/html/vonhirschfeld.eu/niklas/
 git add .
 git commit -m "auto updating site"
 # git push
